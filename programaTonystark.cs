af2a1552b12580379db90817547fd3b94e75e1a2
michelle0234 <arangoarangomichelle@gmail.com>
2024-05-16 14:53:42 -0500

    Creación código Tony Stark
    Crear archivo programa y solución necesidad Tony Stark

---
+++ programaTonystark.cs
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Tony stark esta desarrollando un sistema
+de seguridad para su mansion utilizando
+c#. Complete el espacio en blanco para
+limpiar la pantalla antes de mostrar
+el mensaje de "acceso denegado":
+"
+*******************************************************************************/
+using System;
+class program
+
+         static void Main()
+             
+             console.Clear();
+             Console.WriteLine("Acceso denegado");
+  }
+}
